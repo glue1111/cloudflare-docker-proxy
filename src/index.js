a7be09ec7a5e7a2ce9a67febdd3b4190237cceb2
glue1111 <glue1111@gmail.com>
2024-03-09 19:34:01 +0800

    dns to glue1111.top
    dns to glue1111.top

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.glue1111.top": "https://registry-1.docker.io",
+  "quay.glue1111.top": "https://quay.io",
+  "gcr.glue1111.top": "https://gcr.io",
+  "k8s-gcr.glue1111.top": "https://k8s.gcr.io",
+  "k8s.glue1111.top": "https://registry.k8s.io",
+  "ghcr.glue1111.top": "https://ghcr.io",
+  "cloudsmith.glue1111.top": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
